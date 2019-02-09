81491afbbbff27dae93f4395e5c2f3f78cb05b16
Stefan Stoynov <sstoynov87@gmail.com>
2019-02-09 13:42:14 +0200

    create index page

--- 07. WORKSHOP JAVA EE SERVLETS + JSP + JPA/src/main/java/metube/web/servlets/IndexServlet.java
+++ 07. WORKSHOP JAVA EE SERVLETS + JSP + JPA/src/main/java/metube/web/servlets/IndexServlet.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getRequestDispatcher("/jsp/index.jsp").forward(req, resp);
     }
 }
