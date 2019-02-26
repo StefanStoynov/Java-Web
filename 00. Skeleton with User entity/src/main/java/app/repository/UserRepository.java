78425e23bd9b73da3490af36d379192d7b879d7a
Stefan Stoynov <sstoynov87@gmail.com>
2019-02-26 23:14:01 +0200

    remove folder

--- 00. Skeleton with User entity/src/main/java/app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package app.repository;
-
-import app.domain.entities.User;
-
-public interface UserRepository extends GenericRepository<User,String> {
-    User findByUsername(String username);
-}
--- 00. Skeleton with User entity/src/main/java/app/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package app.service;
-
-import app.domain.models.service.UserServiceModel;
-
-public interface UserService {
-    boolean registerUser(UserServiceModel userServiceModel);
-
-    UserServiceModel loginUser(UserServiceModel userServiceModel);
-}
